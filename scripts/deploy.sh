5571420994ee82e4ff3e3d425c38f097b7cf65bf
Akshat Patel <akshat@live.ca>
2022-11-05 23:30:27 -0400

    Format actions-master3
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,61 +11,60 @@ echo "== Remote git url"
 echo "$(git remote get-url origin | tr -d '\n')"
 
 # deploy to gh pages
-	mkdir -p pages
-	cd pages
+mkdir -p pages
+cd pages
 
-	git init
+git init
 
-	git config user.name "Akshat Patel"
-	git config user.email "akshat@live.ca"
+git config user.name "Akshat Patel"
+git config user.email "akshat@live.ca"
 
-	echo "Git has been configured to use carbon-bot"
+echo "Git has been configured to use carbon-bot"
 
-	git pull "https://git:${GH_TOKEN}@github.com/akshat55/carbon-components-angular.git" gh-pages
+# clone gh-pages branch into workspace
+git pull "https://git:${GH_TOKEN}@github.com/akshat55/carbon-components-angular.git" gh-pages
 
-	echo "carbon-components-angular gh-pages pull successful"
+echo "carbon-components-angular gh-pages pull successful"
 
-	# clean up old build files in the root
-	rm -f *.js
-	rm -f *.map
-	rm -rf documentation
+# clean up old build files in the root
+rm -f *.js
+rm -f *.map
+rm -rf documentation
 
-	mkdir -p documentation
-	cp -R ../dist/docs/documentation/* ./documentation
-	cp -R ../dist/docs/storybook/* ./
+mkdir -p documentation
+cp -R ../dist/docs/documentation/* ./documentation
+cp -R ../dist/docs/storybook/* ./
 
-	version=$(node -e 'const package = require("./../dist/package.json"); console.log(package.version);')
-	mkdir -p $version
-	mkdir -p $version/documentation
-	cp -R ../dist/docs/documentation/* $version/documentation
-	cp -R ../dist/docs/storybook/* $version
-	echo "Version of new package is: $version"
+version=$(node -e 'const package = require("./../dist/package.json"); console.log(package.version);')
+mkdir -p $version
+mkdir -p $version/documentation
+cp -R ../dist/docs/documentation/* $version/documentation
+cp -R ../dist/docs/storybook/* $version
+echo "Version of new package is: $version"
 
-	echo "angular.carbondesignsystem.com" > CNAME
+echo "angular.carbondesignsystem.com" > CNAME
 
-	# in this case we want the script to keep running, so we can actually check the $? (status) var
-	set +e
-	# Commit all the things into the repo
-	git add .
-	git commit -m "Deploy to GitHub Pages"
+# in this case we want the script to keep running, so we can actually check the $? (status) var
+set +e
+# Commit all the things into the repo
+git add .
+git commit -m "Deploy to GitHub Pages"
 
-	echo "Deploy to github pages is good to go"
-	git remote set-url origin "https://git:${GH_TOKEN}@github.com/akshat55/carbon-components-angular.git"
-
-	git remote -v
-
-	git checkout -b gh-pages
+echo "Deploy to github pages is good to go"
+# git remote set-url origin "https://git:${GH_TOKEN}@github.com/akshat55/carbon-components-angular.git"
+git log -1
+git remote -v
 
 
-	# Force push to gh-pages if there was something to commit
-	if [ $? -eq 0 ]; then
+
+# Force push to gh-pages if there was something to commit
+if [ $? -eq 0 ]; then
 	echo "In here"
-		git push --force HEAD:gh-pages
-		echo "Let's see the push"
-		git log -1
-	fi
+	git push --force "https://git:${GH_TOKEN}@github.com/akshat55/carbon-components-angular.git" master:gh-pages
+	echo "Let's see the push"
+fi
 
-	echo "Exit"
+echo "Exit"
 
 # just to be sure we exit cleanly
 exit 0;
